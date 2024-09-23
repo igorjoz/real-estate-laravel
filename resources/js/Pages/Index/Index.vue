3490b7b35eeff357b840850186f66d8a2c4d372a
Igor Jozefowicz <igorjozefowicz@gmail.com>
2024-09-23 10:58:48 +0200

    configure Inertia & Vue
    - create basic routing to index page
    - create IndexController
    - create Index & Show components

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Index page</div>
+    Hello {{ counter }}!
+</template>
+
+<script setup>
+    import {ref} from 'vue';
+
+    const counter = ref(0);
+    setInterval(() => counter.value++, 1000);
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Show page</div>
+</template>
