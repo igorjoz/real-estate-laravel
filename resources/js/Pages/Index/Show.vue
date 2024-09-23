6560cc03416c54fec3ed894c6a0fcda910ae2fd2
Igor Jozefowicz <igorjozefowicz@gmail.com>
2024-09-23 12:25:10 +0200

    develop layout - use MainLayout by default in app.js

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -11,10 +11,3 @@
     defineProps(['message'])
 </script>
 
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue'
-
-    export default {
-        layout: MainLayout
-    }
-</script>
