c27d602e559c5fe7817e878e6520b54bba2a61a8
Igor Jozefowicz <igorjozefowicz@gmail.com>
2024-09-24 12:30:03 +0200

    create Listing: model, migrations, seeder, factory

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'beds' => fake()->numberBetween(1, 5),
+            'baths' => fake()->numberBetween(1, 7),
+            'area' => fake()->numberBetween(30, 400),
+
+            'city' => fake()->city(),
+            'code' => fake()->postcode(),
+            'street_name' => fake()->streetName(),
+            'street_number' => fake()->numberBetween(10, 200),
+
+            'price' => fake()->numberBetween(10_000, 1_000_000),
+        ];
+    }
+}
---
+++ database/seeders/ListingSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Listing;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ListingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // use ListingFactory
+
+        Listing::factory()->count(50)->create();
+    }
+}
