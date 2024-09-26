b0f76e5a5510084984796bd093bec740e6fd652b
Igor Jozefowicz <igorjozefowicz@gmail.com>
2024-09-26 23:09:37 +0200

    create Components: ListingSpace, Price, Box
    - create Listing/Index/Components Listibng.vue

---
+++ resources/js/Components/Price.vue
@@ -0,0 +1,21 @@
+<template>
+    <span>
+        {{ formattedPrice }}
+    </span>
+</template>
+
+<script setup>
+    import { computed } from 'vue'
+
+    const props = defineProps({
+        price: [Number, String],
+    })
+
+    const formattedPrice = computed(() => {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            maximumFractionDigits: 0,
+        }).format(props.price)
+    })
+</script>
